f0ced257ce9158e381e42eaffa7e411398a1f7c9
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2024-08-24 04:49:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144531
+let accountEmail = "gourav@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let acccountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,acccountState])
