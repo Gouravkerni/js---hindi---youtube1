6ef44811d025c072050f5687c90e5357be5e552f
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2024-08-25 19:20:28 +0000

    stack and heap

---
+++ 01_basics/stackAndHeap.js
@@ -0,0 +1,20 @@
+//stack = primitive data type
+//heap = non - primitive data type
+
+let utubechannel = "gouravkerni134@gmail.com"
+let another = utubechannel
+another = "chai aur code"
+
+console.log(utubechannel)
+console.log(another);
+
+let userOne = {
+    email : "gouravkerni134@gmail.com",
+    upi : "usr@pl"
+}
+let userTwo = userOne;
+userTwo.email = "sourav@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
