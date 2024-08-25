d9f5f561d2bdffd1b4255e0525296b31d715c5d6
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2024-08-25 18:30:05 +0000

    conversions.js

---
+++ 01_basics/01_conversions.js
@@ -0,0 +1,30 @@
+// let score = "33"
+
+// console.log(score);
+// console.log(typeof score)
+
+// let valInNumber = Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber);
+
+// let score= 1
+
+// let boolScore = Boolean(score)
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof boolScore);
+// console.log(boolScore);
+
+
+let name = "gourav"
+
+
+console.log(name);
+
+
+
+
+
+
+
+
