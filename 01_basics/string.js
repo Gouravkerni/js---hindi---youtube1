7bd3615429a4f1b528264a0fbdc2a2cb68c0b121
Gourav kerni <137972139+Gouravkerni@users.noreply.github.com>
2024-08-26 17:33:48 +0000

    string.js

---
+++ 01_basics/string.js
@@ -0,0 +1,16 @@
+const gameName = new String("gourav - kerni - done ")
+
+// console.log(gameName[0]);
+// console.log(gameName.charAt(2));
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.substring(0,3));
+// console.log(gameName.slice(-8,-4));
+
+const newString = "    gourav    "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "gorav134@gmail.com"
+console.log(url.replace('r',"ur"));
+console.log(gameName.split(' , '));
